85f68c7e71d747ec6aae8737bf48d40c2c830857
fartuney <102562065+fartuney@users.noreply.github.com>
2022-07-05 17:48:40 -0600

    Forecast added

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast"
 import axios from "axios";
 import "./Weather.css";
 export default function Weather(props) {
@@ -8,6 +9,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
@@ -55,6 +57,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+      <WeatherForecast coordinates={weatherData.coordinates}/>
       </div>
     );
   } else {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="float-left">
-            <Weathericon code={props.data.icon} />
+            <Weathericon code={props.data.icon}  size = {52}/>
           </div>
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
